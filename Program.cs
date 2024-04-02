9f3deea7d6e404e681fd5be6dc69017160d4f12d
Vladyslav Nehodiuk <pearmadn@email.com>
2024-04-02 21:23:19 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using BoardGames_TelegramBot;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+
+internal class Program
+{
+    static TelegramBotClient botClient = new TelegramBotClient("7018047162:AAGZcJIG34hRZkDm15xVlzUF0479eMvZVlo");
+
+    private static async Task Main(string[] args)
+    {
+        await botClient.SetMyCommandsAsync(GetMainCommands());
+
+        botClient.StartReceiving(MessageHandleService.Update, MessageHandleService.Error);
+
+        Console.ReadLine();
+    }
+
+    private static IEnumerable<BotCommand> GetMainCommands()
+    {
+        return new List<BotCommand>()
+        {
+            new BotCommand()
+            {
+                Command = "/game_list",
+                Description = "List of games 🎲"
+            },
+            new BotCommand()
+            {
+                Command = "/vote_for_game",
+                Description = "Vote for game 🗳️"
+            },
+            new BotCommand()
+            {
+                Command = "/stats",
+                Description = "Stats 💾️"
+            },
+            new BotCommand()
+            {
+                Command = "/ban_random_member",
+                Description = "Ban random member 🐭️"
+            },
+        };
+    }
+}
---
+++ Services/MessageHandleService.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.Bot.Types;
+using Telegram.Bot;
+
+namespace BoardGames_TelegramBot
+{
+    public class MessageHandleService
+    {
+        public static async Task Update(ITelegramBotClient botClient, Update update, CancellationToken token)
+        {
+            var message = update.Message;
+            if (message?.Text == null)
+            {
+                return;
+            }
+
+            Console.WriteLine($"Listen: {message.Chat.Username} | Message: {message.Text}");
+
+            if (message.Text.Equals("/game_list"))
+            {
+                await botClient.SendTextMessageAsync(
+               message.Chat.Id,
+               "Game list");
+            }
+
+            //await botClient.SendTextMessageAsync(
+            //    message.Chat.Id,
+            //    "Test text",
+            //    parseMode: ParseMode.MarkdownV2,
+            //    disableNotification: false,
+            //    replyToMessageId: update.Message.MessageId,
+            //    replyMarkup: new InlineKeyboardMarkup(
+            //    InlineKeyboardButton.WithUrl(
+            //        text: "Check sendMessage method",
+            //        url: "https://core.telegram.org/bots/api#sendmessage")
+            //    ));
+
+            await botClient.SendTextMessageAsync(
+                message.Chat.Id,
+                "Підарас",
+                replyToMessageId: update.Message.MessageId);
+
+            return;
+        }
+
+        public static Task Error(ITelegramBotClient arg1, Exception arg2, CancellationToken arg3)
+        {
+            return null;
+        }
+    }
+}
